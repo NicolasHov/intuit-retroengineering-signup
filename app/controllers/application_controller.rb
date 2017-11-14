98d5655c61f10a4d1802aa1e7fe410ccf3e5a388
NicolasHov <kola.hov@gmail.com>
2017-11-14 15:15:35 +0100

    FC

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+ before_action :sanitize_devise_params, if: :devise_controller?
+
+ def sanitize_devise_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone])
+ end
+end
